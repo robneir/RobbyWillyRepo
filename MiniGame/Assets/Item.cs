5e83b0989dd5d7fe2d6680ece5bd964b2afd2ffd
William Corrin <soccerpkmn@gmail.com>
2015-06-11 03:29:55 -0700

    Half-finished item code, + swords / crates

---
+++ MiniGame/Assets/Item.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item : MonoBehaviour 
+{
+	public bool HasUser = false;
+	public Transform SetTran;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(HasUser && !GetComponent<Rigidbody2D>().fixedAngle)
+		{
+			GetComponent<Rigidbody2D>().fixedAngle = true;
+			GetComponent<Transform>().rotation = SetTran.rotation;
+		}
+		else
+		{
+			GetComponent<Rigidbody2D>().fixedAngle = true;
+		}
+	}
+}
---
+++ MiniGame/Assets/PlayerItems.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerItems : MonoBehaviour {
+
+	public GameObject Current = null;
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Current != null)
+		{
+			//you have a item
+			if(Input.GetButtonDown("Fire1"))
+			{
+				//play swing sword animation/fire or whatever
+			}
+
+			if(Input.GetKeyDown(KeyCode.Q))
+			{
+				//throw item
+				if(transform.localScale.x == -1)
+				{
+					Current.GetComponent<Rigidbody2D>().AddForce(Vector3.up * 3);
+					Current.GetComponent<Item>().HasUser = false;
+					Current.GetComponent<SpriteRenderer>().enabled = false;
+				}
+			}
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D c)
+	{
+		//if youre colliding with an item with no user
+		if(c.gameObject.tag.Equals("Item") && !c.gameObject.GetComponent<Item>().HasUser)
+		{
+			//assign item to user
+					//Current = c.gameObject;
+					//Current.GetComponent<SpriteRenderer>().enabled = false;
+			Current.GetComponent<SpriteRenderer>().sprite = c.gameObject.GetComponent<SpriteRenderer>().sprite;
+			Current.GetComponent<SpriteRenderer>().enabled = true;
+			Current.GetComponent<Item>().HasUser = true;
+			GameObject.Destroy(c.gameObject);
+		}
+	}
+}
