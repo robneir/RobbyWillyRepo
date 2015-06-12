d6c47b744684890aff2d2fdba8f94f601ae7aac5
Robert Neir <robneir@gmail.com>
2015-06-12 00:22:22 -0700

    basic powerups

--- MiniGame/Assets/Scripts/PlayerHealth.cs
+++ MiniGame/Assets/Scripts/PlayerHealth.cs
@@ -10,10 +10,6 @@ public class PlayerHealth : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown (KeyCode.L)) {
-			this.GetComponent<Rigidbody2D>().isKinematic=false;
-			this.GetComponent<Rigidbody2D>().fixedAngle=false;
-			this.GetComponentInChildren<Animator>().enabled=false;
-		}
+
 	}
 }
