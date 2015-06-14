2ede26e97c480e4d71b2c0f511d0db5a2b0a7ef6
William Corrin <soccerpkmn@gmail.com>
2015-06-14 15:45:46 -0700

    Added AnimStates script

---
+++ MiniGame/Assets/Scripts/AnimStates.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimStates : MonoBehaviour {
+
+	public enum PlayerAnimationState
+	{
+		Idle,
+		Walking,
+		Running,
+		Swinging,
+		Jumping
+	}
+}
