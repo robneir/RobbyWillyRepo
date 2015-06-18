a7d73710b25c8c45964d33f6356f4022649a681d
Robert Neir <robneir@gmail.com>
2015-06-17 19:44:44 -0700

    tank kinda shooting

---
+++ MiniGame/Assets/TankMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TankMovement : MonoBehaviour {
+
+    public bool xAxisEnabled = false;
+    public bool yAxisEnabled = false;
+    public bool zAxisEnabled = false;
+    public float speed;
+
+    private Rigidbody2D rigidBody2D;
+    private Animator animator;
+    private Vector3 deltaPos;
+
+    // Use this for initialization
+    void Start () {
+        rigidBody2D = GetComponentInChildren<Rigidbody2D>();
+        animator = GetComponentInChildren<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (xAxisEnabled)
+        {
+            deltaPos.x = Input.GetAxis("Horizontal") * speed;
+            rigidBody2D.AddForce(new Vector3(Input.GetAxis("Horizontal") * speed,0,0));
+        }
+        animator.SetFloat("Speed",Mathf.Abs(Input.GetAxis("Horizontal")));
+        this.transform.position += deltaPos;
+    }
+}
---
+++ MiniGame/Assets/TankShoot.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TankShoot : MonoBehaviour {
+
+    public Transform shellspawn;
+    public GameObject shellPrefab;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetButtonDown("Fire1"))
+        {
+            ShootShell();
+        }
+	}
+
+    void ShootShell()
+    {
+       GameObject shell=  (GameObject) Instantiate(shellPrefab, shellspawn.position, shellspawn.rotation);
+       shell.GetComponent<Rigidbody2D>().AddForce(shellspawn.forward * 1000);
+    }
+}
