4adfc69a5fdc484795481e3117de61e51f6d620c
William Corrin <soccerpkmn@gmail.com>
2015-06-19 23:10:59 -0700

    Items restructured, shotgun works

--- MiniGame/Assets/Scripts/Item.cs
+++ MiniGame/Assets/Scripts/Item.cs
@@ -14,9 +14,12 @@ public class Item : MonoBehaviour
 	public ItemType Type;
 	[HideInInspector]
 	public Vector3 Offset;
-
+	[HideInInspector]
+	public Vector3 OriginalScale;
 	public Transform FireTip;
+	[HideInInspector]
     public bool isBeingUsed;
+	public GameObject MuzzleFlash;
 
 	public enum ItemType
 	{
@@ -29,6 +32,8 @@ public class Item : MonoBehaviour
 	// Use this for initialization
 	void Start () 
 	{
+		OriginalScale = transform.localScale;
+
 		switch(Type)
 		{
 			case ItemType.Gun:
@@ -48,7 +53,7 @@ public class Item : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
-		if(HasUser && !GetComponent<Rigidbody2D>().fixedAngle)
+		/*if(HasUser && !GetComponent<Rigidbody2D>().fixedAngle)
 		{
 			GetComponent<Rigidbody2D>().fixedAngle = true;
 			GetComponent<Transform>().rotation = SetTran.rotation;
@@ -57,7 +62,7 @@ public class Item : MonoBehaviour
 		else
 		{
 			GetComponent<Rigidbody2D>().fixedAngle = false;
-		}
+		}*/
 	}
 
 	private void FireShotgun()
@@ -72,17 +77,13 @@ public class Item : MonoBehaviour
 
 	void InstantiateHBullet()
 	{
-		GameObject bull = (GameObject)PhotonNetwork.Instantiate("Bullet", GameObject.FindGameObjectWithTag("Player").transform.position, Quaternion.identity, 0);	
-		bull.GetComponent<Bullet> ().Damage = this.Damage;
-		Debug.Log (Name);
+		GameObject.Destroy((GameObject)GameObject.Instantiate(MuzzleFlash, this.FireTip.position, Quaternion.identity), 1f);	
 
-		if(transform.localScale.x == -1)
+		for(int i = 0; i < 3; ++i)
 		{
-			bull.GetComponent<Rigidbody2D>().velocity = new Vector3(10,0,0);
-		}
-		else
-		{
-			bull.GetComponent<Rigidbody2D>().velocity = new Vector3(-10,0,0);
+			GameObject bull = (GameObject)PhotonNetwork.Instantiate("Bullet", this.FireTip.position, Quaternion.identity, 0);	
+			bull.GetComponent<Bullet> ().Damage = this.Damage;
+			bull.GetComponent<Rigidbody2D>().velocity =  new Vector3(transform.root.localScale.x * 45, (i - 1) * 4, 0);
 		}
 	}
 
